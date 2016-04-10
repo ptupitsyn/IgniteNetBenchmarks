d509c5b2f1aace20ec76c1333cff3f6d409c51a8
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-04-10 10:16:51 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace IgniteNetBenchmarks
+{
+    class Program
+    {
+        static void Main()
+        {
+            BenchmarkRunner.Run<SqlBenchmark>();
+        }
+    }
+}
---
+++ SqlBenchmark.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using Apache.Ignite.Core;
+using Apache.Ignite.Core.Binary;
+using Apache.Ignite.Core.Cache;
+using Apache.Ignite.Core.Cache.Configuration;
+using Apache.Ignite.Core.Cache.Query;
+using BenchmarkDotNet.Attributes;
+
+namespace IgniteNetBenchmarks
+{
+    //[Config("jobs=LongRun")]
+    public class SqlBenchmark
+    {
+        private readonly ICache<int, Person> _cache = Setup();
+
+        //[Setup]
+        public static ICache<int, Person> Setup()
+        {
+            Ignition.StopAll(true);
+
+            var ignite = Ignition.Start(new IgniteConfiguration
+            {
+                BinaryConfiguration = new BinaryConfiguration(typeof (Person))
+            });
+
+            var cache = ignite.CreateCache<int, Person>(new CacheConfiguration("persons", typeof(Person)));
+
+            cache.PutAll(Enumerable.Range(1, 10000).ToDictionary(x => x, x => new Person
+            {
+                Id = x,
+                Data = Guid.NewGuid().ToString(),
+                Name = "Vasya Petin " + x
+            }));
+
+            return cache;
+        }
+
+        [Benchmark]
+        public void IgniteSql()
+        {
+            var sqlQuery = new SqlQuery(typeof (Person), "where id > ? and id < ?", 1000, 1010);
+            var res = _cache.Query(sqlQuery).GetAll();
+
+            if (res.Count != 9)
+                throw new Exception();
+        }
+
+        [Benchmark]
+        public void SqlServer()
+        {
+            
+        }
+    }
+
+    public class Person
+    {
+        [QuerySqlField(IsIndexed = true)]
+        public int Id { get; set; }
+
+        [QuerySqlField]
+        public string Name { get; set; }
+
+        public string Data { get; set; }
+    }
+
+}

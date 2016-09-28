f011b923608ba0753629ebcc36abc9080347c3e0
Pavel Tupitsyn <ptupitsyn@apache.org>
2016-09-28 17:42:06 +0300

    Upgrade to Ignite 1.7.0

---
+++ IgniteSerializationBenchmark.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace IgniteSerializationPerformance
+{
+    public class IgniteSerializationBenchmark
+    {
+        [Benchmark]
+        public void IgniteReflective()
+        {
+            // TODO
+        }
+
+        [Benchmark]
+        public void IgniteReflectiveRaw()
+        {
+            // TODO
+        }
+    }
+}

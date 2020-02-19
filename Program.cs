a05759b8565c355b7d035fb174ccc56cb14c183d
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-02-19 11:12:14 +0300

    Cleanup

--- Program.cs
+++ Program.cs
@@ -6,17 +6,9 @@ namespace IgniteNetBenchmarks
     {
         static void Main()
         {
-            //BenchmarkRunner.Run<SqlServerBenchmark>();
-
-            //BenchmarkRunner.Run<IgniteSqlBenchmark>();
-
-            //BenchmarkRunner.Run<CasterBenchmark>(ManualConfig
-            //    .Create(DefaultConfig.Instance)
-            //    .With(Job.RyuJitX64.WithLaunchCount(1).WithWarmupCount(1)));
-
             //BenchmarkRunner.Run<IgniteSerializationBenchmark>();
-
-            BenchmarkRunner.Run<IgniteLinqBenchmark>();
+            BenchmarkRunner.Run<CasterBenchmark>();
+            //BenchmarkRunner.Run<IgniteLinqBenchmark>();
         }
     }
 }

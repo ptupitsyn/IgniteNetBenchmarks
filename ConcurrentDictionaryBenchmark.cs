8e4ec89a5a82060beba0097474463139522a1f63
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-02-19 11:46:00 +0300

    Adding concurrent dictionary benches

---
+++ ConcurrentDictionaryBenchmark.cs
@@ -0,0 +1,13 @@
+using System.Collections.Concurrent;
+
+namespace IgniteNetBenchmarks
+{
+    /// <summary>
+    /// Compares generic vs non-generic <see cref="ConcurrentDictionary{TKey,TValue}"/> usage.
+    /// For Native Near Cache feature.
+    /// </summary>
+    public class ConcurrentDictionaryBenchmark
+    {
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# IgniteNetBenchmarks
-Apache Ignite.NET benchmarks
+# Ignite.NET Benchmarks
+
+Various benchmarks related to [Ignite.NET](https://ignite.apache.org) development. 

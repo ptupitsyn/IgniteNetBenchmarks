ee361d78d4d3e5aaf506ce3c1f4ff5077e58774f
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-10-27 11:12:09 +0300

    Add ScanQuery benchmark with PlatformCache

--- PlatformCacheBenchmark.cs
+++ PlatformCacheBenchmark.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Apache.Ignite.Core;
 using Apache.Ignite.Core.Cache;
 using Apache.Ignite.Core.Cache.Configuration;
+using Apache.Ignite.Core.Cache.Query;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Jobs;
 
@@ -36,6 +39,24 @@ namespace IgniteNetBenchmarks
                 throw new Exception();
         }
 
+        [Benchmark]
+        public void CacheQueryScan()
+        {
+            var res = _cache.Query(new ScanQuery<int, Person>(new Filter())).Single().Value;
+
+            if (res.Id != 42)
+                throw new Exception();
+        }
+
+        [Benchmark(Baseline = true)]
+        public void CacheQueryScanWithPlatform()
+        {
+            var res = _cacheWithPlatformCache.Query(new ScanQuery<int, Person>(new Filter())).Single().Value;
+
+            if (res.Id != 42)
+                throw new Exception();
+        }
+
         [GlobalSetup]
         public void GlobalSetup()
         {
@@ -53,15 +74,27 @@ namespace IgniteNetBenchmarks
                 }
             });
 
-            var person = Person.CreateInstance<Person>(42);
-            _cache[1] = person;
-            _cacheWithPlatformCache[1] = person;
+            var data = Enumerable.Range(1, 10000)
+                .Select(Person.CreateInstance<Person>)
+                .Select(p => new KeyValuePair<int, Person>(p.Id, p))
+                .ToArray();
+
+            _cache.PutAll(data);
+            _cacheWithPlatformCache.PutAll(data);
         }
 
         [GlobalCleanup]
         public void GlobalCleanup()
         {
             _ignite.Dispose();
+        }
+
+        private class Filter : ICacheEntryFilter<int, Person>
+        {
+            public bool Invoke(ICacheEntry<int, Person> entry)
+            {
+                return entry.Key == 42;
+            }
         }
     }
 }
